cdf3e602052b0ff95a7082a6017a3d8f37c7b202
VincentContreraz <vcontreraz132@gmail.com>
2024-09-26 19:45:56 -0700

    added definition for LAN_addr_len and IP_addr_len to prevent possible future confusion of values

--- headers.h
+++ headers.h
@@ -2,11 +2,13 @@
 #define HEADERS_H
 #include <stdint.h>
 
+#define LAN_addr_len 6 //define ethernet address size of 6 bytes
+#define IP_addr_len 4 //define IP address size of 4 bytes
 
 // ethernet header struct
 struct enet_header {
-	uint8_t dest_addr[6];
-	uint8_t src_addr[6];
+	uint8_t dest_addr[LAN_addr_len];
+	uint8_t src_addr[LAN_addr_len];
 	uint16_t enet_type;
 }__attributes__((packed));
 
@@ -17,8 +19,10 @@ struct arp_header {
 	uint8_t hlen;
 	uint8_t plen;
 	uint16_t opcode;
-	uint8_t src_mac[6];
-	uint8_t src_ip[4];
-	uint8_t dest_mac[6];
-	uint8_t dest_ip[4];
+	uint8_t src_mac[LAN_addr_len];
+	uint8_t src_ip[IP_addr_len];
+	uint8_t dest_mac[LAN_addr_len];
+	uint8_t dest_ip[IP_addr_len];
 }__attributes__((packed));
+
+
