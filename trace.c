d1e3b73cbbf2e1334f630b9b60c766346a11998b
VincentContreraz <vcontreraz132@gmail.com>
2024-09-29 08:06:35 -0700

    fixed packet parsing function to check ethernet type field to determine what header type to parse next

--- trace.c
+++ trace.c
@@ -9,12 +9,29 @@
 		(void)user_data;
 
 		packet_num++;
-
+		
 		printf("Packet number: %d  Packet Len: %d\n\n",packet_num, header->len);
+
+		struct enet_header *enet = (struct enet_header *)packet;
 
 		ethernet_parser(packet);
 		packet = packet + 14; // ethernet header is 14 bytes long, advance pointer
-		arp_parser(packet);
+		
+		switch(ntohs(enet->enet_type)) {
+			case 0x0800:
+				ip_parser(packet);
+				struct IP_header *ip_head = (struct IP_header *)packet;
+				uint8_t ip_head_len = (ip_head->version_IHL & 0x0F) * 4; // calc IP header length by masking verison and IHL field
+				packet = packet + ip_head_len; // advance packet pointer
+				break;
+			case 0x0806:
+				arp_parser(packet);
+				packet = packet + 28; // ARP headers are 28 bytes
+				break;
+			default:
+				printf("Error: Uknown header type");
+				break;
+		}
 	}
 
 	int main(int argc, char *argv[]) {
@@ -40,6 +57,7 @@
 
 		// int ex_return = pcap_next_ex(handle, &header, &packet); // pcap_next_ex returns a value based on success or failure
 		// instead of pcap_next_ex, use pcap_loop to read all packets
+		printf("\n");
 		int loop_return = pcap_loop(handle, -1, packet_parser, NULL);
 
 
@@ -50,7 +68,6 @@
 		}
 
 		pcap_close(handle);
-		printf("Trace file closed\n");
 
 		return 0;
 	}
